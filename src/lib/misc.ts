793a655bd0996e74b8acb9bdf01a6fb073ed6c9a
Melon56 <121840186+Melon56@users.noreply.github.com>
2023-03-25 09:46:28 -0700

    fixed spelling mistake in misc.ts

--- src/lib/misc.ts
+++ src/lib/misc.ts
@@ -1,19 +1,22 @@
-export function browser(): "firefox" | "chromium" | "webkit" | "uknown" {
+export function parseHTML(input: string | string[]) {
+ if (typeof input === "object") {
+  input.forEach(item => {
+   item.replace(">", "&gt;").replace("<", "&lt;").replace("&", "&amp;").replace('"', "&quot;").replace("'", "&apos;");
+  });
+ } else {
+  input.replace(">", "&gt;").replace("<", "&lt;").replace("&", "&amp;").replace('"', "&quot;").replace("'", "&apos;");
+ };
+ return input;
+};
+
+function getBrowser(): "firefox" | "chromium" | "webkit" | "unknown" {
  ///@ts-ignore
  if (typeof window.InstallTrigger === "object") return "firefox";
  ///@ts-ignore
  else if (typeof window.chrome === "object") return "chromium";
  ///@ts-ignore
- else if (/Safari/.test(navigator.userAgent)) return "webkit";
- else return "uknown";
+ else if (document.webkitExitFullscreen) return "webkit";
+ else return "unknown";
 };
-export function parseHTML(input: string|string[]) {
- if (typeof input === "object") {
-  input.forEach(item => {
-   item.replace(">", "&gt;").replace("<", "&lt;").replace("&", "&amp;").replace('"', "&quot;").replace("'", "&apos;");
-  });
- } else { 
-  input.replace(">", "&gt;").replace("<", "&lt;").replace("&", "&amp;").replace('"', "&quot;").replace("'", "&apos;");
- };
- return input;
-};
+
+export const browser = getBrowser();
