ca855b1b6855203ee48e35426a8ce9cb36cbf73d
Melon56 <121840186+Melon56@users.noreply.github.com>
2023-04-06 20:10:02 -0700

    debugging stuff for firefox

--- src/lib/acronyms.ts
+++ src/lib/acronyms.ts
@@ -1,9 +1,10 @@
 export const icon = document.querySelector<HTMLLinkElement>("link[rel='icon']").href;
 export const browser = (() => {
  ///@ts-ignore
- if (typeof window.mozRTCSessionDescription !== "undefined") return "firefox";
+ if (typeof document.body.mozMatchesSelector === "function") return "firefox";
  ///@ts-ignore
- else if (typeof window.chrome !== "undefined") return "chromium";
- else if (/Safari/.test(navigator.userAgent)) return "webkit";
+ else if (typeof chrome === "object") return "chromium";
+ ///@ts-ignore
+ else if (typeof elem.webkitRequestFullscreen === "function") return "webkit";
  else return "unknown";
 })()
