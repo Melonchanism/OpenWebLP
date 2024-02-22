b600c5aabe98fc34a23df082e15e233a885892c7
josh <121840186+Melonchanism@users.noreply.github.com>
2024-02-22 13:43:23 -0800

    replaced variables with stores for better reactivity

--- src/lib/menu/routes/Service.svelte
+++ src/lib/menu/routes/Service.svelte
@@ -1,66 +1,72 @@
 <script lang="ts">
-//@ts-ignore
-import Sortable from 'sortablejs'
-import {slide} from "svelte/transition";
-import {allSongs, myServiceStore} from "$lib/songs";
-import {onMount} from "svelte";
-import {Service} from "$lib/types";
+  import Sortable from "sortablejs";
+  import { slide } from "svelte/transition";
+  import { allSongs, myServiceStore } from "$lib/songs";
+  import { onMount } from "svelte";
+  import { Service } from "$lib/types";
 
-let listElm: HTMLDivElement;
-let listElm2: HTMLDivElement;
+  let listElm: HTMLDivElement;
+  let listElm2: HTMLDivElement;
 
-let list: Sortable;
+  let list: Sortable;
 
-let search = $state("");
+  let search = $state("");
 
-const myService = $myServiceStore;
+  const myService = $myServiceStore;
 
-onMount(() => {
-	new Sortable(listElm, {
-		group: {
-			name: "songs",
-			pull: "clone",
-			put: false
-    },
-    sort: false,
-    animation: 150,
-  })
-  list = new Sortable(listElm2, {
-		group: "songs",
-		removeOnSpill: true,
-		animation: 150,
-    ghostClass: "moving",
-    onAdd: createService,
-	  onRemove: createService,
-    onMove: createService,
-    onSpill: () => setTimeout(createService, 300)
-	})
-})
-function createService() {
-	setTimeout(() => {
-		let output: number[] = []
-		list.toArray().forEach(item => {
-			output.push(parseInt(item))
-		})
-		myServiceStore.set(new Service("auto", output))
-  }, 50)
-}
+  onMount(() => {
+    new Sortable(listElm, {
+      group: {
+        name: "songs",
+        pull: "clone",
+        put: false,
+      },
+      sort: false,
+      animation: 150,
+    });
+    list = new Sortable(listElm2, {
+      group: "songs",
+      removeOnSpill: true,
+      animation: 150,
+      ghostClass: "moving",
+      onAdd: createService,
+      onRemove: createService,
+      onMove: createService,
+      onSpill: () => setTimeout(createService, 300),
+    });
+  });
+  function createService() {
+    setTimeout(() => {
+      let output: number[] = [];
+      list.toArray().forEach((item) => {
+        output.push(parseInt(item));
+      });
+      myServiceStore.set(new Service("auto", output));
+    }, 50);
+  }
 </script>
 
 <div class="main">
   <div class="column">
     <h2>All</h2>
-    <input type="text" placeholder="Search" on:keydown|stopPropagation={() => {}} bind:value={search} />
+    <input
+      type="text"
+      placeholder="Search"
+      on:keydown|stopPropagation={() => {}}
+      bind:value={search}
+    />
     <div class="list" bind:this={listElm}>
-      {#each allSongs as song}
-        {#if (`${song.name} ${song.artist}`).toLowerCase().search(search.toLowerCase()) !== -1}
+      {#each $allSongs as song}
+        {#if `${song.name} ${song.artist}`
+          .toLowerCase()
+          .search(search.toLowerCase()) !== -1}
           <div
             class="item"
-            data-id={allSongs.indexOf(song)}
-            transition:slide={{duration: 300}}
+            data-id={$allSongs.indexOf(song)}
+            transition:slide={{ duration: 300 }}
           >
             {song.name}
-            <br>
+            <br />
             {song.artist}
           </div>
         {/if}
@@ -72,9 +78,9 @@ function createService() {
     <div class="list" bind:this={listElm2}>
       {#each myService.songs as index}
         <div class="item" data-id={index}>
-          {allSongs[index].name}
-          <br>
-          {allSongs[index].artist}
+          {$allSongs[index].name}
+          <br />
+          {$allSongs[index].artist}
         </div>
       {/each}
     </div>
@@ -82,33 +88,34 @@ function createService() {
 </div>
 
 <style lang="less">
-div.main {
-  display: grid;
-  grid-template-columns: 50% 50%;
-  max-height: inherit;
-  h2 {
-    text-align: center;
-  }
-  input {
-    background: #222222c5;
-    font-size: 20px;
-    margin-left: 8px;
-    border-radius: 8px;
-    padding: 4px;
-    width: calc(100% - 24px);
-  }
-  div.column {
+  div.main {
+    display: grid;
+    grid-template-columns: 50% 50%;
     max-height: inherit;
-    overflow: scroll;
-    div.list {
-      min-height: 60vh;
-      display: flex;
-      flex-direction: column;
+    h2 {
+      text-align: center;
+    }
+    input {
+      background: #222222c5;
+      font-size: 20px;
+      margin-left: 8px;
+      border-radius: 8px;
+      padding: 4px;
+      width: calc(100% - 24px);
+    }
+    div.column {
+      max-height: inherit;
+      overflow-y: scroll;
+      overflow-x: hidden;
+      div.list {
+        min-height: 60vh;
+        display: flex;
+        flex-direction: column;
+      }
     }
   }
-}
-:global(div.item.moving) {
-  transition: opacity 300ms;
-  opacity: 0.2;
-}
+  :global(div.item.moving) {
+    transition: opacity 300ms;
+    opacity: 0.2;
+  }
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,126 +1,180 @@
 <script lang="ts">
-import Preview from "$lib/Preview.svelte";
-import Menu from "$lib/menu/Menu.svelte";
-import {blur} from "svelte/transition";
-import {onMount} from "svelte";
-import "../styles.less"
-import {allSongs, myServiceStore} from "$lib/songs";
+  import Preview from "$lib/Preview.svelte";
+  import Menu from "$lib/menu/Menu.svelte";
+  import { blur } from "svelte/transition";
+  import { onMount } from "svelte";
+  import "../styles.less";
+  import { allSongs, myServiceStore } from "$lib/songs";
 
-const channel = new BroadcastChannel("key")
+  const channel = new BroadcastChannel("key");
 
-let myService = $state($myServiceStore);
+  let currentSong = $state(0);
+  let currentLyric = $state(0);
 
-let currentSong = $state(0);
-let currentLyric = $state(0);
+  let isBlank = false;
 
-let isBlank = false
-
-onMount(() => {
-	$effect(() => {
-		if (myService.songs.length > 0 && allSongs[myService.songs[currentSong]]) {
-			const lyric = allSongs[myService.songs[currentSong]].lyrics[currentLyric]
-			if (!isBlank) {
-				localStorage.setItem("lyricInfo", `${lyric.type} ${lyric.number}`)
-				localStorage.setItem("songInfo", `${allSongs[myService.songs[currentSong]].name} | ${allSongs[myService.songs[currentSong]].artist}`);
-				localStorage.setItem("lyric", lyric.text);
-      }
-			document.querySelectorAll("div.lyrics button.item, div.songs button.item")?.forEach(itm => itm.classList.remove("current"));
-			document.querySelectorAll("div.lyrics button.item")?.item(currentLyric)?.classList.add("current");
-			document.querySelectorAll("div.lyrics button.item")?.item(currentLyric)?.scrollIntoView({ behavior: "smooth", block: "center", inline: "nearest" });
-			document.querySelectorAll("div.songs button.item")?.item(currentSong)?.classList.add("current");
-			document.querySelectorAll("div.songs button.item")?.item(currentSong)?.scrollIntoView({ behavior: "smooth", block: "center", inline: "nearest" });
-		} else if (myService.songs.length > 0) {
-			currentSong = 0;
-      currentLyric = 0;
-    } else {
-			localStorage.setItem("lyricInfo", "")
-			localStorage.setItem("songInfo", "");
-			localStorage.setItem("lyric", "")
-    }
-  })
-  myServiceStore.subscribe(() => {
-		myService = $myServiceStore
-  })
-  channel.addEventListener("message", evt => handleKey(evt.data[0], undefined, evt.data[1]))
-})
-
-function handleKey(key: string, evt?: KeyboardEvent, shift?: boolean) {
-	switch (key) {
-    case "ArrowDown":
-	    evt?.preventDefault()
-	    !shift ?
-		    allSongs[myService.songs[currentSong]].lyrics[currentLyric + 1] ? currentLyric++ : null :
-		    allSongs[myService.songs[currentSong]].lyrics[currentLyric + 2] ? currentLyric += 2 : null;
-			break;
-		case "ArrowUp":
-			evt?.preventDefault()
-			!shift ?
-        allSongs[myService.songs[currentSong]].lyrics[currentLyric - 1] ? currentLyric-- : null :
-        allSongs[myService.songs[currentSong]].lyrics[currentLyric - 2] ? currentLyric -= 2 : null;
-			break;
-    case "ArrowRight":
-			if (allSongs[myService.songs[currentSong + 1]]) {
-				currentLyric = 0;
-				currentSong++;
-      }
-			break;
-		case "ArrowLeft":
-			if (allSongs[myService.songs[currentSong - 1]]) {
-				currentLyric = 0;
-				currentSong--;
-      }
-			break;
-    case "b":
-	    isBlank = !isBlank
-	    if (isBlank) {
-		    localStorage.setItem("lyricInfo", "")
-		    localStorage.setItem("songInfo", "");
-		    localStorage.setItem("lyric", "")
+  onMount(() => {
+    $effect(() => {
+      if (
+        $myServiceStore.songs.length > 0 &&
+        $allSongs[$myServiceStore.songs[currentSong]]
+      ) {
+        const lyric =
+          $allSongs[$myServiceStore.songs[currentSong]].lyrics[currentLyric];
+        if (!isBlank) {
+          localStorage.setItem("lyricInfo", `${lyric.type} ${lyric.number}`);
+          localStorage.setItem(
+            "songInfo",
+            `${$allSongs[$myServiceStore.songs[currentSong]].name} | ${$allSongs[$myServiceStore.songs[currentSong]].artist}`,
+          );
+          localStorage.setItem("lyric", lyric.text);
+        }
+        document
+          .querySelectorAll("div.lyrics button.item, div.songs button.item")
+          ?.forEach((itm) => itm.classList.remove("current"));
+        document
+          .querySelectorAll("div.lyrics button.item")
+          ?.item(currentLyric)
+          ?.classList.add("current");
+        document
+          .querySelectorAll("div.lyrics button.item")
+          ?.item(currentLyric)
+          ?.scrollIntoView({
+            behavior: "smooth",
+            block: "center",
+            inline: "nearest",
+          });
+        document
+          .querySelectorAll("div.songs button.item")
+          ?.item(currentSong)
+          ?.classList.add("current");
+        document
+          .querySelectorAll("div.songs button.item")
+          ?.item(currentSong)
+          ?.scrollIntoView({
+            behavior: "smooth",
+            block: "center",
+            inline: "nearest",
+          });
+      } else if ($myServiceStore.songs.length > 0) {
+        currentSong = 0;
+        currentLyric = 0;
       } else {
-		    const lyric = allSongs[myService.songs[currentSong]].lyrics[currentLyric]
-		    localStorage.setItem("lyricInfo", `${lyric.type} ${lyric.number}`)
-		    localStorage.setItem("songInfo", `${allSongs[myService.songs[currentSong]].name} by: ${allSongs[myService.songs[currentSong]].artist}`);
-		    localStorage.setItem("lyric", lyric.text);
+        localStorage.setItem("lyricInfo", "");
+        localStorage.setItem("songInfo", "");
+        localStorage.setItem("lyric", "");
       }
-      break;
-    case "f":
-			document.querySelector("iframe")?.requestFullscreen()
-      break;
-    case "d":
-			open("/display", "_blank", "popup")
-      break;
-	}
-}
+    });
+    channel.addEventListener("message", (evt) =>
+      handleKey(evt.data[0], undefined, evt.data[1]),
+    );
+  });
+
+  function handleKey(key: string, evt?: KeyboardEvent, shift?: boolean) {
+    switch (key) {
+      case "ArrowDown":
+        evt?.preventDefault();
+        !shift
+          ? $allSongs[$myServiceStore.songs[currentSong]].lyrics[
+              currentLyric + 1
+            ]
+            ? currentLyric++
+            : null
+          : $allSongs[$myServiceStore.songs[currentSong]].lyrics[
+                currentLyric + 2
+              ]
+            ? (currentLyric += 2)
+            : null;
+        break;
+      case "ArrowUp":
+        evt?.preventDefault();
+        !shift
+          ? $allSongs[$myServiceStore.songs[currentSong]].lyrics[
+              currentLyric - 1
+            ]
+            ? currentLyric--
+            : null
+          : $allSongs[$myServiceStore.songs[currentSong]].lyrics[
+                currentLyric - 2
+              ]
+            ? (currentLyric -= 2)
+            : null;
+        break;
+      case "ArrowRight":
+        if ($allSongs[$myServiceStore.songs[currentSong + 1]]) {
+          currentLyric = 0;
+          currentSong++;
+        }
+        break;
+      case "ArrowLeft":
+        if ($allSongs[$myServiceStore.songs[currentSong - 1]]) {
+          currentLyric = 0;
+          currentSong--;
+        }
+        break;
+      case "b":
+        isBlank = !isBlank;
+        if (isBlank) {
+          localStorage.setItem("lyricInfo", "");
+          localStorage.setItem("songInfo", "");
+          localStorage.setItem("lyric", "");
+        } else {
+          const lyric =
+            $allSongs[$myServiceStore.songs[currentSong]].lyrics[currentLyric];
+          localStorage.setItem("lyricInfo", `${lyric.type} ${lyric.number}`);
+          localStorage.setItem(
+            "songInfo",
+            `${$allSongs[$myServiceStore.songs[currentSong]].name} by: ${$allSongs[$myServiceStore.songs[currentSong]].artist}`,
+          );
+          localStorage.setItem("lyric", lyric.text);
+        }
+        break;
+      case "f":
+        document.querySelector("iframe")?.requestFullscreen();
+        break;
+      case "d":
+        open("/display", "_blank", "popup");
+        break;
+    }
+  }
 </script>
 
-<svelte:window on:keydown={evt => handleKey(evt.key, evt, evt.shiftKey)} />
+<svelte:window on:keydown={(evt) => handleKey(evt.key, evt, evt.shiftKey)} />
 
 <div class="main">
   <div class="grid-item songs" style:grid-area="songs">
-    {#each myService.songs as index}
+    {#each $myServiceStore.songs as index}
       <button
         class="item"
-        on:click={evt => {
+        on:click={(evt) => {
           currentLyric = 0;
-          currentSong = Array.from(evt.target?.parentElement?.querySelectorAll("button")).indexOf(evt.target);
+          currentSong = Array.from(
+            // @ts-ignore
+            evt.target?.parentElement?.querySelectorAll("button"),
+          ).indexOf(evt.target);
         }}
-        transition:blur={{duration: 300}}
+        transition:blur={{ duration: 300 }}
       >
-        {allSongs[index].name}
-        <br>
-        {allSongs[index].artist}
+        {$allSongs[index].name}
+        <br />
+        {$allSongs[index].artist}
       </button>
     {/each}
   </div>
   <div class="grid-item lyrics" style:grid-area="lyrics">
-    {#if myService.songs.length > 0 && allSongs[myService.songs[currentSong]]}
-      {#each allSongs[myService.songs[currentSong]].lyrics as lyric}
+    {#if $myServiceStore.songs.length > 0 && $allSongs[$myServiceStore.songs[currentSong]]}
+      {#each $allSongs[$myServiceStore.songs[currentSong]].lyrics as lyric}
         <button
           class="item"
-          on:click={evt => currentLyric = Array.from(evt.target?.parentElement?.querySelectorAll("button")).indexOf(evt.target)}
+          on:click={(evt) =>
+            (currentLyric = Array.from(
+              // @ts-ignore
+              evt.target?.parentElement?.querySelectorAll("button"),
+            ).indexOf(evt.target))}
         >
-          {lyric.type} {lyric.number}
-          <br>
+          {lyric.type}
+          {lyric.number}
+          <br />
           {lyric.text}
         </button>
       {/each}
@@ -134,20 +188,20 @@ function handleKey(key: string, evt?: KeyboardEvent, shift?: boolean) {
 <Menu />
 
 <style lang="less">
-@gridItem: calc(50% - 5px);
-.main {
-  display: grid;
-  height: 100%;
-  grid-template-areas: "songs lyrics" "preview lyrics";
-  grid-template-rows: @gridItem @gridItem;
-  grid-template-columns: @gridItem @gridItem;
-  gap: 10px;
-  .grid-item {
-    display: flex;
-    flex-direction: column;
-    border: var(--border-color) solid 2px;
-    border-radius: 4px;
-    overflow: scroll;
+  @gridItem: calc(50% - 5px);
+  .main {
+    display: grid;
+    height: 100%;
+    grid-template-areas: "songs lyrics" "preview lyrics";
+    grid-template-rows: @gridItem @gridItem;
+    grid-template-columns: @gridItem @gridItem;
+    gap: 10px;
+    .grid-item {
+      display: flex;
+      flex-direction: column;
+      border: var(--border-color) solid 2px;
+      border-radius: 4px;
+      overflow: scroll;
+    }
   }
-}
 </style>
