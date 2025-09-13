c6aed49aa8f7ba907c35f5e0da1eac667f59f3cc
Melonchanism <121840186+Melonchanism@users.noreply.github.com>
2025-09-13 14:54:01 -0700

    refactor: add better type checking in +server.ts

--- src/routes/edit/+server.ts
+++ src/routes/edit/+server.ts
@@ -1,13 +1,16 @@
+import type { Song } from "$lib/localStorage.js"
 import { supabase } from "$lib/supabase.server"
 import { json } from "@sveltejs/kit"
 
 export async function POST({ request }) {
-	const requestData = await request.json()
+	const requestData: Song = await request.json()
 
 	if (requestData.id !== -1) {
 		const { error } = await supabase.from("songs").update(requestData).eq("id", requestData.id)
+		if (error) throw error
 		return new Response(null, { status: 202 })
 	} else {
+		//@ts-expect-error
 		delete requestData.id
 		const { data, error } = await supabase.from("songs").insert(requestData).select("id")
 		if (error) throw error
