da6a715691ca8891430d2ca5deba216bc5d09c3d
Melonchanism <121840186+Melonchanism@users.noreply.github.com>
2026-08-05 14:47:49 -0700

    refactor: cleanup unneeded dependencies

--- svelte.config.js
+++ svelte.config.js
@@ -1,21 +1,17 @@
 import adapter from "@sveltejs/adapter-vercel"
-import { sveltePreprocess } from "svelte-preprocess"
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte"
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
-	preprocess: sveltePreprocess({
-		scss: {
-			prependData: `@use "src/styles.scss" as *;`,
-		},
-	}),
+	preprocess: vitePreprocess({ script: true }),
 	kit: {
 		adapter: adapter(),
 	},
 	onwarn: (warning, handler) => {
 		const { code } = warning
-		if (code === "css_unused_selector") {
+		if (code === "css_unused_selector" || code === "a11y_missing_content") {
 			return
 		}
 
